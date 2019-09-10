1d502552ec6bddf11b986c17be79fbb52a05d876
guachinw <andrek_kl@yahoo.com.pe>
2019-09-09 21:48:56 -0500

    asd

---
+++ Assets/Agent.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent : MonoBehaviour
+{
+    Vector3 velocity = Vector3.zero;
+    Vector3 desired = Vector3.zero;
+
+    Vector3 steer = Vector3.zero;
+    float maxSpeed = 5;
+    float maxSteer = 10;
+
+    Transform goal;
+
+    Transform target;
+    void Start()
+    {
+        goal = GameObject.Find("Goal").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        target = GameObject.Find("Target").transform;
+
+        //desired/deseado
+        desired = -(target.position - transform.position).normalized * maxSpeed; 
+        steer = Vector3.ClampMagnitude(desired - velocity, maxSteer);
+
+        velocity += steer * Time.deltaTime; //direccion
+        transform.position += velocity * Time.deltaTime;
+
+        float dist = Vector3.Distance(goal.position, transform.position);
+
+        if(dist <= 1f)
+        {
+            Debug.Log("Win");
+            Destroy(gameObject);
+        }
+        else if (transform.position.x >= 10 || transform.position.x <= -10){
+            Debug.Log("Lose");
+            Destroy(gameObject);
+        }
+
+        else if (transform.position.y >= 5 || transform.position.y <= -5){
+            Debug.Log("Lose");
+            Destroy(gameObject);
+        }
+
+
+    }
+    void OnBecameInvinsible()
+    {
+        Debug.Log("Lose");
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Target.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //float tx = 10;
+    //float ty = 20;
+
+    /*float tx = 0;
+    float ty = 0;
+    float minX = -5;
+    float miny = -5;
+    float width = 10;
+    float height = 10;*/
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*tx += 0.01f;
+        ty += 0.01f;
+
+        //float x = minX + width * Mathf.PerlinNoise(tx, 0);
+        //float y = miny + height * Mathf.PerlinNoise(ty,0);
+
+        float x = -5 + 10 * Mathf.PerlinNoise(tx, 0);
+		float y = -5 + 10 * Mathf.PerlinNoise(0, ty);
+
+
+        transform.position = new Vector3(x,y,0);*/
+
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.Scale(new Vector3(1,1,0));
+        transform.position = mousePosition;
+    }
+}
